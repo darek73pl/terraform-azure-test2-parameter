d5ea1595859be5f0870fe58c5b2dfdcbac38f08d
darek73pl <dkurzynski@gazeta.pl>
2019-12-09 13:51:25 +0100

    correcting beckend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "Terraform-RG"
     storage_account_name = "terraformstorageaccount1"
-    container_name       = "terraform-state-test2"
+    container_name       = "terraform-state-test2-parameter"
     key                  = "terraform.tfstate"
   }
 }
